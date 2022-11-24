7406ac749ce409006a590dfdd3e7a172185cc584
dkalopitas <kalopitas.dhmhtrhs@gmail.com>
2022-11-24 23:05:19 +0200

    Create UserController and implement getAllUsers()

--- backend/src/main/java/com/auction_website/backend/controller/UserController.java
+++ backend/src/main/java/com/auction_website/backend/controller/UserController.java
@@ -1,2 +1,23 @@
-package com.auction_website.backend.controller;public class UserController {
+package com.auction_website.backend.controller;
+
+import com.auction_website.backend.model.User;
+import com.auction_website.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
