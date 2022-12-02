1df79c9d5febf034e5eb9fc8fb0d0c67d0155141
dkalopitas <kalopitas.dhmhtrhs@gmail.com>
2022-12-02 21:56:28 +0200

    Make SignUpComponent functional component

---
+++ frontend/src/components/SignUpComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SignUpComponent() {
+    
+    return (
+        <div>
+            <h1>Sign-Up</h1>
+        </div>
+    );
+}
+
+export default SignUpComponent;
