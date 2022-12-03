a619659da38cfa6c01167378d3b5b62136e84c62
dkalopitas <kalopitas.dhmhtrhs@gmail.com>
2022-12-03 22:22:51 +0200

    Change navigate hook to Link in HomepageComponent

--- frontend/src/components/HomepageComponent.js
+++ frontend/src/components/HomepageComponent.js
@@ -1,13 +1,12 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom"
 
 function HomepageComponent() {
-    const navigate = useNavigate();
 
     return(
         <div className="container">
             <h1>Homepage</h1>
-            <button className='btn btn-primary' onClick={() => {navigate("/sign-up")}}>Sign Up</button>
+            <Link to="/sign-up" className='btn btn-primary'>Sign Up</Link>
         </div>
     );
 }
