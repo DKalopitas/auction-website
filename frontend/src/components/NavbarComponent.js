e04336ed1d50cc7690debbe0a30997f480095c41
dkalopitas <kalopitas.dhmhtrhs@gmail.com>
2022-12-06 21:13:17 +0200

    Remove bootstrap package and add bootstrap css and js in index.html by cdn. Also make navbar toggler work when window is small

--- frontend/src/components/NavbarComponent.js
+++ frontend/src/components/NavbarComponent.js
@@ -4,13 +4,33 @@ import { Link } from "react-router-dom";
 export default function NavbarComponent() {
 
     return(
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
+        
+        <nav className="navbar sticky-top navbar-expand-sm navbar-dark bg-dark">
             <div className="container-fluid px-5">
-                <Link to="/" className="navbar-brand">Home</Link>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul className="nav nav-pills mr-auto" style={{fontSize: 18}}>
-                        <li className="nav-item"><Link to="/users" className="nav-link">Users</Link></li>
-                        <li className="nav-item"><Link to="/sign-up" className="nav-link">Sign Up</Link></li>
+                <Link to="/" className="navbar-brand mb-0 h1">Home</Link>
+                <button 
+                type="button"
+                className="navbar-toggler"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav"
+                     style={{fontSize: 18}}>
+                        <li className="nav-item">
+                            <Link to="/users" className="nav-link">
+                                Users
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/sign-up" className="nav-link">
+                                Sign Up
+                            </Link>
+                        </li>
                     </ul>
                 </div>
             </div>
