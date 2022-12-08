b99da8fc750d9772954465bd20ae6416aa53a99e
dkalopitas <kalopitas.dhmhtrhs@gmail.com>
2022-12-09 01:03:28 +0200

    Edit Backend UserRepository

--- backend/src/main/java/com/auction_website/backend/repository/UserRepository.java
+++ backend/src/main/java/com/auction_website/backend/repository/UserRepository.java
@@ -2,8 +2,12 @@ package com.auction_website.backend.repository;
 
 import com.auction_website.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
+@Transactional(readOnly = true)
 public interface UserRepository  extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
