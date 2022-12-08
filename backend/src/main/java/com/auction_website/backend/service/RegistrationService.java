3aed3823a8559fcd9a015767129d0bece0498aac
dkalopitas <kalopitas.dhmhtrhs@gmail.com>
2022-12-09 01:25:26 +0200

    Create backend RegistrationService

---
+++ backend/src/main/java/com/auction_website/backend/service/RegistrationService.java
@@ -0,0 +1,9 @@
+package com.auction_website.backend.service;
+
+import com.auction_website.backend.request.RegistrationRequest;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return null;
+    }
+}
