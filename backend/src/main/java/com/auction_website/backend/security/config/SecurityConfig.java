a5b148ae602a90a1890adabec5f4313db8825550
dkalopitas <kalopitas.dhmhtrhs@gmail.com>
2022-12-09 03:32:52 +0200

    Fix the PasswordEncoder.java

---
+++ backend/src/main/java/com/auction_website/backend/security/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.auction_website.backend.security.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("api/v*/registration/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                        .and()
+                        .formLogin()
+                );
+        return http.build();
+    }
+
+}
