4a33a174e6142c6a8e41824ed1357245a99d20d2
dkalopitas <kalopitas.dhmhtrhs@gmail.com>
2022-12-09 23:28:30 +0200

    Edit UserController and add to it AllArgsConstructor from lombok, rename getAllUsers method to getUsers and inside access the data from userService. Also comment out the PostMapping

--- backend/src/main/java/com/auction_website/backend/controller/UserController.java
+++ backend/src/main/java/com/auction_website/backend/controller/UserController.java
@@ -1,27 +1,30 @@
 package com.auction_website.backend.controller;
 
 import com.auction_website.backend.model.User;
-import com.auction_website.backend.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.auction_website.backend.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/v1/")
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserService userService;
+
+//    @Autowired
+//    private UserRepository userRepository;
 
     @GetMapping("/users")
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
+    public List<User> getUsers() {
+        return userService.getUsers();
     }
 
-    @PostMapping("/users")
-    public User createUser(@RequestBody User user) {
-        return userRepository.save(user);
-    }
+//    @PostMapping("/users")
+//    public User createUser(@RequestBody User user) {
+//        return userRepository.save(user);
+//    }
+    
 }
