2e0eaa956f9bc26fce1788e73a5698a2f65a9000
dkalopitas <kalopitas.dhmhtrhs@gmail.com>
2022-12-09 03:34:13 +0200

    Fix RegistrationService that is missing @Service

--- backend/src/main/java/com/auction_website/backend/service/RegistrationService.java
+++ backend/src/main/java/com/auction_website/backend/service/RegistrationService.java
@@ -1,9 +1,11 @@
 package com.auction_website.backend.service;
 
 import com.auction_website.backend.request.RegistrationRequest;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RegistrationService {
     public String register(RegistrationRequest request) {
-        return null;
+        return "works";
     }
 }
