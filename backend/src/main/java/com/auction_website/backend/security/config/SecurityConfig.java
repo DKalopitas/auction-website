9c1b62ee48c7c5cd366ec825a5941dc58df1a1cb
dkalopitas <kalopitas.dhmhtrhs@gmail.com>
2022-12-09 03:36:08 +0200

    Rename WebSecurityConfig to SecurityConfig and fix the securityFilterChain method and make the daoAuthanticationProvider method

--- backend/src/main/java/com/auction_website/backend/security/config/SecurityConfig.java
+++ backend/src/main/java/com/auction_website/backend/security/config/SecurityConfig.java
@@ -1,30 +1,60 @@
 package com.auction_website.backend.security.config;
 
+import com.auction_website.backend.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class SecurityConfig {
+
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> {
+            try {
+                authorize
                         .requestMatchers("api/v*/registration/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
                         .and()
-                        .formLogin()
-                );
+                        .formLogin();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
         return http.build();
+    }
+
+    // TODO: Fix the old @override configure(AuthenticationManagerBuilder)
+
+//    public SecurityFilterChain securityFilterChain(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.authenticationProvider(daoAuthenticationProvider());
+//        return null;
+//    }
+
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.authenticationProvider(daoAuthenticationProvider());
+//    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(userService);
+        return provider;
     }
 
 }
