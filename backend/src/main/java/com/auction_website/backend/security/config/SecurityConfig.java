8bf859ab96b0f012d0e4af0e6967240409f8c478
dkalopitas <kalopitas.dhmhtrhs@gmail.com>
2022-12-09 22:47:29 +0200

    Edit securityFilterChain in SecurityConfig

--- backend/src/main/java/com/auction_website/backend/security/config/SecurityConfig.java
+++ backend/src/main/java/com/auction_website/backend/security/config/SecurityConfig.java
@@ -4,13 +4,14 @@ import com.auction_website.backend.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @AllArgsConstructor
@@ -22,20 +23,17 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> {
-            try {
-                authorize
+        return http
+//                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize ->
+                        authorize
                         .requestMatchers("api/v*/registration/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
-                        .and()
-                        .formLogin();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-        return http.build();
+                )
+                .formLogin(withDefaults())
+                .build();
     }
 
     // TODO: Fix the old @override configure(AuthenticationManagerBuilder)
