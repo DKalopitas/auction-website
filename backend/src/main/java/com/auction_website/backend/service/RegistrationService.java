88bcb2e455cde0fb03dea3fab8ee744c55c70e06
dkalopitas <kalopitas.dhmhtrhs@gmail.com>
2022-12-18 19:49:25 +0200

    Add initializeDatabase() to RegistrationService to hardwrite an admin user in database

--- backend/src/main/java/com/auction_website/backend/service/RegistrationService.java
+++ backend/src/main/java/com/auction_website/backend/service/RegistrationService.java
@@ -2,8 +2,12 @@ package com.auction_website.backend.service;
 
 import com.auction_website.backend.model.User;
 import com.auction_website.backend.model.UserRole;
+import com.auction_website.backend.repository.UserRepository;
 import com.auction_website.backend.request.RegistrationRequest;
 import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +17,7 @@ public class RegistrationService {
 
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
     public String register(RegistrationRequest request) {
         return userService.signUpUser(new User(
@@ -27,4 +32,27 @@ public class RegistrationService {
                 UserRole.USER
         ), passwordEncoder);
     }
+
+    @Bean
+    public CommandLineRunner initializeDatabase() {
+        if (userRepository.findByUsername("admin").isPresent()) {
+            return args -> {};
+        }
+        return args -> {
+            User admin = new User(
+                    "admin",
+                    "admin",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    UserRole.ADMIN
+            );
+            admin.setEnabled(true);
+            userService.signUpUser(admin, passwordEncoder);
+        };
+    }
+
 }
