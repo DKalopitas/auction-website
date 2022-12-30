235780441c8899e16fae1ab8c7dc250e01293dba
dkalopitas <kalopitas.dhmhtrhs@gmail.com>
2022-12-30 21:25:15 +0200

    Make navbar links rendered responsive based on roles

--- frontend/src/components/LogInComponent.js
+++ frontend/src/components/LogInComponent.js
@@ -81,33 +81,35 @@ const LogInComponent = () => {
                                 <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
                                 <p className="text-white-50 mb-5">Please enter your username and password!</p>
                                 
-                                <div className="form-outline form-white mb-4">
-                                    <label className="form-label" htmlFor="username">Username</label>
-                                    <input type="text"
-                                        id="username"
-                                        ref={usernameRef}
-                                        autoComplete="off"
-                                        onChange={(e) => setUsername(e.target.value)}
-                                        value={username}
-                                        required 
-                                        className="form-control form-control-lg"
-                                    />
-                                </div>
+                                <form onSubmit={handleSubmit}>
+                                    <div className="form-outline form-white mb-4">
+                                        <label className="form-label" htmlFor="username">Username</label>
+                                        <input type="text"
+                                            id="username"
+                                            ref={usernameRef}
+                                            autoComplete="off"
+                                            onChange={(e) => setUsername(e.target.value)}
+                                            value={username}
+                                            required 
+                                            className="form-control form-control-lg"
+                                        />
+                                    </div>
 
-                                <div className="form-outline form-white mb-4">
-                                    <label className="form-label" htmlFor="typePasswordX">Password</label>
-                                    <input type="password" 
-                                        id="typePasswordX"
-                                        onChange={(e) => setPassword(e.target.value)}
-                                        value={password}
-                                        required
-                                        className="form-control form-control-lg"
-                                    />
-                                </div>
+                                    <div className="form-outline form-white mb-4">
+                                        <label className="form-label" htmlFor="typePasswordX">Password</label>
+                                        <input type="password" 
+                                            id="typePasswordX"
+                                            onChange={(e) => setPassword(e.target.value)}
+                                            value={password}
+                                            required
+                                            className="form-control form-control-lg"
+                                        />
+                                    </div>
 
-                                <p className="small mb-5 pb-lg-2"><a className="text-white-50" href="#!">Forgot password?</a></p>
+                                    <p className="small mb-5 pb-lg-2"><a className="text-white-50" href="#!">Forgot password?</a></p>
 
-                                <button className="btn btn-outline-light btn-lg px-5" type="submit" onClick={handleSubmit}>Login</button>
+                                    <button className="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+                                </form>
 
                                 <div ref={errorRef} 
                                 className={errorMsg ? "errmsg" : "offscreen"}
--- frontend/src/components/NavbarComponent.js
+++ frontend/src/components/NavbarComponent.js
@@ -1,10 +1,58 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 export default function NavbarComponent() {
+    const { setAuth } = useAuth();
+    const { auth } = useAuth();
+
+    function handleLogOut() {
+        setAuth({});
+    }
+
+    function logInController() {
+        if (auth.roles === 'USER') {
+            return(
+                <li className="nav-item">
+                    <Link to="/" className="nav-link" onClick={handleLogOut}>
+                        Log Out
+                    </Link>
+                </li>
+            )
+        }
+        if (auth.roles === 'ADMIN') {
+            return(
+                <React.Fragment>
+                    <li className="nav-item">
+                        <Link to="/users" className="nav-link">
+                            Users
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/" className="nav-link" onClick={handleLogOut}>
+                            Log Out
+                        </Link>
+                    </li>
+                </React.Fragment>
+            )
+        }
+        return(
+            <React.Fragment>
+                <li className="nav-item">
+                    <Link to="/log-in" className="nav-link">
+                        Log In
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/sign-up" className="nav-link">
+                        Sign Up
+                    </Link>
+                </li>
+            </React.Fragment>
+        )
+    }
 
     return(
-        
         <nav className="navbar sticky-top navbar-expand-sm navbar-dark bg-dark">
             <div className="container-fluid px-5">
                 <Link to="/" className="navbar-brand mb-0 h1">Home</Link>
@@ -15,27 +63,16 @@ export default function NavbarComponent() {
                 data-bs-target="#navbarNav"
                 aria-controls="navbarNav"
                 aria-expanded="false"
-                aria-label="Toggle navigation">
+                aria-label="Toggle navigation"
+                >
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav"
-                     style={{fontSize: 18}}>
-                        <li className="nav-item">
-                            <Link to="/users" className="nav-link">
-                                Users
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/log-in" className="nav-link">
-                                Log In
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/sign-up" className="nav-link">
-                                Sign Up
-                            </Link>
-                        </li>
+                    <ul 
+                    className="navbar-nav"
+                    style={{fontSize: 18}}
+                    >
+                        {logInController()}
                     </ul>
                 </div>
             </div>
