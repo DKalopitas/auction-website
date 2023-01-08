b47537c075105613458c6f6ba5859d0486182e54
dkalopitas <kalopitas.dhmhtrhs@gmail.com>
2023-01-08 20:00:43 +0200

    Change style of dropdown

--- frontend/src/components/NavbarComponent.js
+++ frontend/src/components/NavbarComponent.js
@@ -27,10 +27,10 @@ export default function NavbarComponent() {
                         </ul>
                         <ul className="navbar-nav fs-5 align-items-center">
                             <li className="nav-item dropdown">
-                                <button className="btn btn-link nav-link py-2 px-0 px-lg-2 dropdown-toggle d-flex align-items-center show" data-bs-toggle="dropdown">
+                                <button className="btn btn-dark nav-link py-2 px-0 px-lg-2 dropdown-toggle d-flex align-items-center show" data-bs-toggle="dropdown">
                                     <i className="fas fa-user"></i>
                                 </button>
-                                <ul className="dropdown-menu dropdown-menu-end text-center">
+                                <ul className="dropdown-menu dropdown-menu-end dropdown-menu-dark text-center">
                                     <li>
                                         <Link to="/profile" className="dropdown-item">
                                             Profile
@@ -61,10 +61,10 @@ export default function NavbarComponent() {
                         </ul>
                         <ul className="navbar-nav fs-5 align-items-center">
                             <li className="nav-item dropdown">
-                                <button className="btn btn-link nav-link py-2 px-0 px-lg-2 dropdown-toggle d-flex align-items-center show" data-bs-toggle="dropdown">
+                                <button className="btn btn-dark nav-link py-2 px-0 px-lg-2 dropdown-toggle d-flex align-items-center show" data-bs-toggle="dropdown">
                                     <i className="fas fa-user"></i>
                                 </button>
-                                <ul className="dropdown-menu dropdown-menu-end text-center">
+                                <ul className="dropdown-menu dropdown-menu-end dropdown-menu-dark text-center">
                                     <li>
                                         <Link to="/profile" className="dropdown-item">
                                             Profile
@@ -104,7 +104,7 @@ export default function NavbarComponent() {
 
     return(
         <nav className="navbar sticky-top navbar-expand-sm navbar-dark bg-dark">
-            <div className="container-fluid px-5">
+            <div className="container-fluid px-5 py-1">
                 <Link to="/" className="navbar-brand mb-0 h1">Home</Link>
                 <button 
                 type="button"
